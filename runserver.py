2d0d7ebcbc70fd9ea9cf906acf9476239f6d5b15
vasanthkumarbalu <61037212+vasanthkumarbalu@users.noreply.github.com>
2020-02-19 14:06:29 +0200

    Add files via upload

--- runserver.py
+++ runserver.py
@@ -2,4 +2,4 @@ from notejam import app
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
+    app.run(host="0.0.0.0",port=80,debug=False,use_reloader=True)
